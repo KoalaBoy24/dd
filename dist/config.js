f5889a3841aef4dec1aff38485c78f0096fc0aae
KoalaBoy24 <79370359+KoalaBoy24@users.noreply.github.com>
2022-04-15 11:42:06 +0300

    Delete dist directory

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-import { readFile, writeFile } from "fs/promises";
-const config = JSON.parse((await readFile("config.json")).toString());
-function saveConfig() {
-    return writeFile("config.json", JSON.stringify(config));
-}
-export default config;
-export { saveConfig };
